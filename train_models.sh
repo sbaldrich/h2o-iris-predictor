3ed9e775f55df806a85e1de6fcd00171fec1eb3e
Santiago Baldrich <santiago.baldrich@gmail.com>
2016-11-11 14:57:51 -0500

    Modified the model creation script to avoid problems with bash

--- train_models.sh
+++ train_models.sh
@@ -5,7 +5,7 @@ mkdir tmp
 echo "Training the random forest and generating its jar file"
 r -f iris_random_forest.R
 cd tmp
-cat <(echo -e package com.baldrichcorp.ml.generated\;\\n) RFIrisPredictor.java > tmp.java
+echo package com.baldrichcorp.ml.generated\; | cat - RFIrisPredictor.java > tmp.java
 mv tmp.java RFIrisPredictor.java
 javac -d . RFIrisPredictor.java -cp h2o-genmodel.jar
 jar cf RFIrisPredictor.jar com
@@ -15,7 +15,7 @@ cd ..
 echo "Training the random forest and generating its jar file"
 r -f iris_gbm.R
 cd tmp
-cat <(echo -e package com.baldrichcorp.ml.generated\;\\n) GBMIrisPredictor.java > tmp.java
+echo package com.baldrichcorp.ml.generated\; | cat - GBMIrisPredictor.java > tmp.java
 mv tmp.java GBMIrisPredictor.java
 javac -d . GBMIrisPredictor.java -cp h2o-genmodel.jar
 jar cf GBMIrisPredictor.jar com
